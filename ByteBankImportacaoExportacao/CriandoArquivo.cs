e99af7f62f0d74b82d3dcec478f22206fbc2aae2
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-19 23:11:39 -0300

    criando arquivo csv

---
+++ ByteBankImportacaoExportacao/CriandoArquivo.cs
@@ -0,0 +1,27 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.IO;
+using System.Text;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void CriandoArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "456, 78945, 4785.50, Gustavo Santos";
+
+                var encoding = Encoding.UTF8;
+
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+
+        }
+    }
+}
--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -1,61 +1,21 @@
 ﻿using ByteBankImportacaoExportacao.Modelos;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 using System.IO; //IO = Input and Output
 
 namespace ByteBankImportacaoExportacao
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            var endereco = "contas.txt";
 
-
-            using (var fluxoDoArquivo = new FileStream(endereco, FileMode.Open))
-            using (var leitor = new StreamReader(fluxoDoArquivo))
-            {
-                while (!leitor.EndOfStream)
-                {
-                    var linha = leitor.ReadLine();
-
-                    var contaCorrente = ConverterContaCorrente(linha);
-
-                    var mensagem = $"{contaCorrente.Titular.Nome}: Conta número {contaCorrente.Numero}, ag. {contaCorrente.Agencia}. Saldo {contaCorrente.Saldo}";
-
-                    Console.WriteLine(mensagem);
-                }
-            }
-
+            CriandoArquivo();
 
             Console.ReadLine();
         }
 
-        private static ContaCorrente ConverterContaCorrente(string linha)
-        {
-            var campos = linha.Split(',');
 
-            var agencia = campos[0];
-            var numero = campos[1];
-            var saldo = campos[2].Replace('.', ',');
-            var nomeTitular = campos[3];
-
-            var titular = new Cliente();
-            titular.Nome = nomeTitular;
-
-            var agenciaInt = int.Parse(agencia);
-            var numeroInt = int.Parse(numero);
-            var saldoDouble = double.Parse(saldo);
-
-            var resultado = new ContaCorrente(agenciaInt, numeroInt);
-            resultado.Depositar(saldoDouble);
-            resultado.Titular = titular;
-
-            return resultado;
-        }
     }
 }
