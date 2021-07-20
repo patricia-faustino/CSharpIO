9d22d53a0332d21ef7dc51f46648eda7feae5b48
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-19 21:40:36 -0300

    Inicializando projeto e criando um FileStream

---
+++ ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,46 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; //IO = Input and Output
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var endereco = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(endereco, FileMode.Open );
+
+            var buffer = new byte[1024]; // 1kb
+
+            var numeroDeBytesLidos = -1;
+
+            while(numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            
+
+            
+
+            Console.ReadLine();
+        }
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+        }
+    }
+} 
+ 
