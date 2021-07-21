1317a7e3ee6ae8124ddbbda145a4be1dbb863e82
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-20 21:35:15 -0300

    utilizando classe file para manipular arquivos

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -10,6 +10,22 @@ namespace ByteBankImportacaoExportacao
     {
         static void Main(string[] args)
         {
+            //utilizar esses métodos caso não estoure a memória do computador
+            File.WriteAllText("escrevendoComAClasseFile.txt", "Testado File.WriteAllText");
+            Console.WriteLine("Arquivo escrevendoComAClasseFile.txt criado");
+
+
+            var bytes = File.ReadAllBytes("contas.txt");
+            Console.WriteLine($"Arquivo contas.txt possui {bytes.Length} bytes");
+
+            //lê todas as linhas do arquivo
+            var linhas = File.ReadAllLines("contas.txt");
+            Console.WriteLine(linhas.Length);
+
+            foreach (var linha in linhas)
+            {
+                Console.WriteLine(linha);
+            }
 
             LeituraBinaria();
             Console.WriteLine("Aplicacao Finalizada.....");
